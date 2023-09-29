6a6855d90db2a3feee55715c4bfc2d84ba6f2ad1
Alexis <al.dacosta@f5.com>
2023-09-29 13:07:51 +0200

    plain text

--- versions.tf
+++ versions.tf
@@ -10,10 +10,10 @@ terraform {
   }
 
   required_providers {
-    volterra = {
-      source  = "volterraedge/volterra"
-      version = "= 0.11.22"
-    }
+#    volterra = {
+#      source  = "volterraedge/volterra"
+#      version = "= 0.11.22"
+#    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.51.0"

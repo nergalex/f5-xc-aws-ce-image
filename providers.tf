04605ede563d4b171935d8c8712b944a9b742458
Alexis <al.dacosta@f5.com>
2023-09-29 13:02:47 +0200

    plain text

--- providers.tf
+++ providers.tf
@@ -5,9 +5,9 @@ provider "volterra" {
   alias         = "default"
 }
 
-provider "aws" {
-  region = var.f5xc_aws_region
-#  access_key = var.aws_access_key_id
-#  secret_key = var.aws_secret_access_key
-  alias  = "default"
-}
+#provider "aws" {
+#  region = var.f5xc_aws_region
+##  access_key = var.aws_access_key_id
+##  secret_key = var.aws_secret_access_key
+#  alias  = "default"
+#}

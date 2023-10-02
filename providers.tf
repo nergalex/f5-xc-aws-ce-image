54dfb9dd251e6f8e2c42b36463c4a23046c4b0e8
Alexis <al.dacosta@f5.com>
2023-10-02 10:25:03 +0200

    test user ID

--- providers.tf
+++ providers.tf
@@ -9,6 +9,5 @@ provider "aws" {
   region      = var.f5xc_aws_region
   access_key  = var.aws_access_key_id
   secret_key  = var.aws_secret_access_key
-  token       = var.aws_secret_access_key
   alias       = "default"
 }

4af1cafd5541e0ad3757c403c3b24480fe5ef6a7
Alexis <al.dacosta@f5.com>
2023-10-02 13:20:18 +0200

    show vpc id

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+#provider "volterra" {
+#  url           = var.f5xc_api_url
+#  api_cert      = var.f5xc_api_cert
+#  api_key       = var.f5xc_api_key
+#  alias         = "default"
+#}
+
 provider "aws" {
   region      = "eu-west-3"
   access_key  = var.aws_access_key_id
   secret_key  = var.aws_secret_access_key
+  alias       = "default"
 }
 
 data "aws_availability_zones" "available" {

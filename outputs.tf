d00e4bafbb0b0dc79f1bd805366bb798c6d75cae
Alexis <al.dacosta@f5.com>
2023-10-02 13:39:10 +0200

    show vpc id

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,6 @@
 #  value = module.f5xc_aws_secure_ce_single_node_single_nic_existing_vpc
 #}
 output "vpc__multi_node_single_nic_existing_vpc_existing_subnet" {
+  version = "2023.10.02-13:38"
   value = module.vpc__multi_node_single_nic_existing_vpc_existing_subnet
 }

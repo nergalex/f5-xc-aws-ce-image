9ce5c98012ab7259f95cadf47337e4675814fb07
Alexis <al.dacosta@f5.com>
2023-10-02 12:14:05 +0200

    show vpc id

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region      = var.f5xc_aws_region
   access_key  = var.aws_access_key_id
   secret_key  = var.aws_secret_access_key
-  alias       = "default"
 }
 
 data "aws_availability_zones" "available" {

dd21e72a4f01fa58f2b4c8b51cca90d130507cb2
Alexis <al.dacosta@f5.com>
2023-10-02 21:16:01 +0200

    comment role

--- modules/f5xc/ce/aws/outputs.tf
+++ modules/f5xc/ce/aws/outputs.tf
@@ -1,12 +1,12 @@
 output "ce" {
   value = {
     vpc_id = var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : null
-    iam    = {
-      role             = aws_iam_role.role
-      policy           = aws_iam_policy.policy
-      attachment       = aws_iam_role_policy_attachment.attachment
-      instance_profile = aws_iam_instance_profile.instance_profile
-    }
+#    iam    = {
+#      role             = aws_iam_role.role
+#      policy           = aws_iam_policy.policy
+#      attachment       = aws_iam_role_policy_attachment.attachment
+#      instance_profile = aws_iam_instance_profile.instance_profile
+#    }
     ssh_key = aws_key_pair.aws_key
     nlb     = length(var.f5xc_aws_vpc_az_nodes) == 3 ? {
       nlb = module.network_nlb[0].nlb

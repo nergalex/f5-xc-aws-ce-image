8eb04a4bdd9886120a16a6481dc8bb2e53baf2b9
Alexis <al.dacosta@f5.com>
2023-10-02 11:10:06 +0200

    show account id

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
-output "f5xc_aws_secure_ce_single_node_single_nic_existing_vpc" {
-  value = module.f5xc_aws_secure_ce_single_node_single_nic_existing_vpc
+#output "f5xc_aws_secure_ce_single_node_single_nic_existing_vpc" {
+#  value = module.f5xc_aws_secure_ce_single_node_single_nic_existing_vpc
+#}
+output "vpc__multi_node_single_nic_existing_vpc_existing_subnet" {
+  value = module.vpc__multi_node_single_nic_existing_vpc_existing_subnet
 }

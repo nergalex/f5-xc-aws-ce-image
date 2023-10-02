2c068589e840a09b9ecdee0680082f9c1227359f
Alexis <al.dacosta@f5.com>
2023-10-02 13:46:42 +0200

    show vpc id

--- modules/aws/vpc/outputs.tf
+++ modules/aws/vpc/outputs.tf
@@ -4,7 +4,8 @@ output "aws_vpc" {
     arn                 = aws_vpc.vpc.arn
     tags                = aws_vpc.vpc.tags
     owner               = aws_vpc.vpc.owner_id
-    igw_id              = var.create_igw ? aws_internet_gateway.igw[0].id : null
+#    igw_id              = var.create_igw ? aws_internet_gateway.igw[0].id : null
+    create_igw          = var.create_igw # Change
     cidr_block          = aws_vpc.vpc.cidr_block
     dhcp_options_id     = aws_vpc.vpc.dhcp_options_id
     main_route_table_id = aws_vpc.vpc.main_route_table_id

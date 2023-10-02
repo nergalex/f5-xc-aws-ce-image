4d01665207e2628b9e7490aa666c486592981629
Alexis <al.dacosta@f5.com>
2023-10-02 14:10:22 +0200

    all modules

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-#provider "volterra" {
-#  url           = var.f5xc_api_url
-#  api_cert      = var.f5xc_api_cert
-#  api_key       = var.f5xc_api_key
-#  alias         = "default"
-#}
+provider "volterra" {
+  url           = var.f5xc_api_url
+  api_cert      = var.f5xc_api_cert
+  api_key       = var.f5xc_api_key
+  alias         = "default"
+}
 
 provider "aws" {
   region      = var.f5xc_aws_region
@@ -25,7 +25,6 @@ module "vpc__multi_node_single_nic_existing_vpc_existing_subnet" {
     aws = aws.default
   }
 }
-
 
 module "f5xc_aws_secure_ce_single_node_single_nic_existing_vpc" {
   source                = "./modules/f5xc/ce/aws"

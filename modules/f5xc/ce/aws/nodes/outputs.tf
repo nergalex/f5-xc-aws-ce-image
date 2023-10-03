f076054e86837c2566886a42f5a64fb18d02c850
Alexis <al.dacosta@f5.com>
2023-10-03 14:01:27 +0200

    comment node

--- modules/f5xc/ce/aws/nodes/outputs.tf
+++ modules/f5xc/ce/aws/nodes/outputs.tf
@@ -1,11 +1,13 @@
 output "ce" {
   value = {
-    id          = aws_instance.instance.id
-    ami         = aws_instance.instance.ami
-    name        = aws_instance.instance.tags["Name"]
-    tags        = aws_instance.instance.tags
-    public_ip   = aws_instance.instance.public_ip
-    private_ip  = aws_instance.instance.private_ip
-    private_dns = aws_instance.instance.private_dns
+#    id          = aws_instance.instance.id
+#    ami         = aws_instance.instance.ami
+#    name        = aws_instance.instance.tags["Name"]
+#    tags        = aws_instance.instance.tags
+#    public_ip   = aws_instance.instance.public_ip
+#    private_ip  = aws_instance.instance.private_ip
+#    private_dns = aws_instance.instance.private_dns
+#    user_data_base64 = aws_instance.instance.user_data_base64
+    user_data_base64     = base64encode(var.f5xc_instance_config)
   }
 }
